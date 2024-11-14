bcb7fdd8442d5cd8d55ddefa2b2aadc45efd2b07
Bruno <brunobromo321@gmail.com>
2024-11-14 11:20:07 -0300

    feat - modal nos cursos

--- src/views/Courses/courses.vue
+++ src/views/Courses/courses.vue
@@ -6,14 +6,35 @@
             <div class="row mt-5">
                 <!-- Cards (Lado a Lado) -->
                 <div class="col-12 col-sm-6 col-md-4 col-lg-4 mb-4 d-flex justify-content-center">
-                <div class="card" style="width: 100%;">
-                    <img src="/images/Home/notebook.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <div class="card" style="width: 100%;">
+                        <img src="/images/Home/notebook.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        Launch demo modal
+                        </button>
+
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    ...
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Save changes</button>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        </div>
                     </div>
-                </div>
                 </div>
         
                 <div class="col-12 col-sm-6 col-md-4 col-lg-4 mb-4 d-flex justify-content-center">
